4218a9c52904860ee421b88cbf5cc3c3807ff6f7
Kisalay <kisalaykisu@gmail.com>
2022-06-20 13:40:08 +0530

    Welcome commit

---
+++ src/com/bridgelabz/LogicalPrograms.java
@@ -0,0 +1,8 @@
+//Welcome Message
+package com.bridgelabz;
+
+public class LogicalPrograms {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the playground of coding");
+    }
+}
