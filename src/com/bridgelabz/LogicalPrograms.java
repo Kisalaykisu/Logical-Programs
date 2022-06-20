491df7e6fea53d0cfeca9e9a585ac49f1e67d02a
Kisalay <kisalaykisu@gmail.com>
2022-06-20 13:55:03 +0530

    2nd commit

--- src/com/bridgelabz/LogicalPrograms.java
+++ src/com/bridgelabz/LogicalPrograms.java
@@ -1,25 +1,34 @@
-/* Q-1. Fibonacci Series
-Fibonacci series is a special type of series in which the next term is the sum of the
-previous two terms. For example, if 0 and 1 are the two previous terms in a series, then
-the next term will be 1(0+1).
-package com.bridgelabz;
+/* Q-2)  Perfect Number
+a. Just like the Armstrong number, the Perfect Number is also a special type of
+positive number. When the number is equal to the sum of its positive divisors
+excluding the number, it is called a Perfect Number. For example, 28 is the perfect
+number because when we sum the divisors of 28, it will result in the same number.
+The divisors of 28 are 1, 2, 4, 7, and 14. So,
+b. 28 = 1+2+4+7
+c. 28 = 28
 */
 package com.bridgelabz;  // Package
 import java.util.Scanner;   // Import Scanner
-import java.util.SplittableRandom; //Import SplittableRandom
+
 
 public class LogicalPrograms {
     public static void main(String[] args) {  // Entry point of Program
-        int a=0,b=1,sum;  //Initializing
-        Scanner sc = new Scanner(System.in);  //Make new Scanner obj
-        System.out.println("Enter the value of terms n: ");
-        int n = sc.nextInt();  // Input int
+        Scanner scanner = new Scanner(System.in);  // make Scanner obj
+        System.out.println("Enter a number: ");
+        int num = scanner.nextInt();  //Input int
+        int sum = 0;
+        System.out.println("The divisors are ");
 
-        for(int i=0;i<n;i++){
-            System.out.println(a);
-            sum=a+b;
-            a=b;
-            b=sum;
+        for(int i=1; i<num ;i++){
+            if (num%i==0){
+                sum+=i;
+                System.out.println(i);
+            }
         }
-    }
+        if (sum==num){
+            System.out.println("So "+num + " is a perfect number");
+        }else
+            System.out.println("So "+num+" is not a perfect number");
+        }
+
 }
