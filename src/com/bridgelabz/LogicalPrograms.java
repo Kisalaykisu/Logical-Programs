a1567366d7ff2b7569d86b2fdb21c4cca205a051
Kisalay <kisalaykisu@gmail.com>
2022-06-20 13:47:52 +0530

    1st commit

--- src/com/bridgelabz/LogicalPrograms.java
+++ src/com/bridgelabz/LogicalPrograms.java
@@ -1,8 +1,25 @@
-//Welcome Message
+/* Q-1. Fibonacci Series
+Fibonacci series is a special type of series in which the next term is the sum of the
+previous two terms. For example, if 0 and 1 are the two previous terms in a series, then
+the next term will be 1(0+1).
 package com.bridgelabz;
+*/
+package com.bridgelabz;  // Package
+import java.util.Scanner;   // Import Scanner
+import java.util.SplittableRandom; //Import SplittableRandom
 
 public class LogicalPrograms {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the playground of coding");
+    public static void main(String[] args) {  // Entry point of Program
+        int a=0,b=1,sum;  //Initializing
+        Scanner sc = new Scanner(System.in);  //Make new Scanner obj
+        System.out.println("Enter the value of terms n: ");
+        int n = sc.nextInt();  // Input int
+
+        for(int i=0;i<n;i++){
+            System.out.println(a);
+            sum=a+b;
+            a=b;
+            b=sum;
+        }
     }
 }
