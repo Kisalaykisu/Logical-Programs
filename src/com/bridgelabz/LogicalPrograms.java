3ca91ed507d2bde7d3c26564754d151dfd87f8b6
Kisalay <kisalaykisu@gmail.com>
2022-06-20 14:38:03 +0530

    6th commit

--- src/com/bridgelabz/LogicalPrograms.java
+++ src/com/bridgelabz/LogicalPrograms.java
@@ -1,32 +1,41 @@
-/* Q-5. Coupon Numbers
-a. Desc -> Given N distinct Coupon Numbers, how many random numbers do you
-need to generate a distinct coupon number? This program simulates this random
-process.
-b. I/P -> N Distinct Coupon Number
-c. Logic -> repeatedly choose a random number and check whether it's a new one.
-d. O/P -> total random number needed to have all distinct numbers.
-e. Functions => Write Class Static Functions to generate random numbers and to
-process distinct coupons.
+/* Q-6. Simulate Stopwatch Program
+a. Desc -> Write a Stopwatch Program for measuring the time that elapses between
+the start and end clicks
+b. I/P -> Start the Stopwatch and End the Stopwatch
+c. Logic -> Measure the elapsed time between start and end
+d. O/P -> Print the elapsed time.
 */
 package com.bridgelabz;  // Package
-import java.util.Scanner;   // Import Scanner
 
+import java.time.Duration; // time duration
+import java.time.Instant;  // the specific time instant on the current timeline
+import java.util.Locale;   // a specific geographical, political, or cultural region.
+import java.util.Scanner;  // Import scanner
 
 public class LogicalPrograms {
-    public static void main(String[] args) {  // Entry point of Program
-        char[] chars="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789".toCharArray(); // toCharArray() method converts the given string into a sequence of characters.
-        int max=100000000;
-        int random=(int) (Math.random()*max);  //random function
-        StringBuffer sb=new StringBuffer(); // StringBuffer is a peer class of String that provides much of the functionality of strings.
+        static Instant start;  // Starting point
+        static Instant stop;   // Ending Point
 
-        while (random>0)
-        {
-            sb.append(chars[random % chars.length]);
-            random /= chars.length;
+        public static void main(String[] args) {  // Entry point of the program
+
+            Scanner scanner = new Scanner(System.in);  //  Make Scanner obj
+            System.out.println("Enter S to start the stop watch: ");
+            char variable = (scanner.next().toUpperCase(Locale.ROOT)).charAt(0);  //root locale is the locale whose language, country, and variant are empty ("") strings.
+            if (variable == 'S') {
+                start = Instant.now();
+                System.out.println("StopWatch has started..");
+            }
+            Scanner scanner2 = new Scanner(System.in);
+            System.out.println("Enter S again to stop the stop watch: ");
+            char variable2 = (scanner.next().toUpperCase(Locale.ROOT)).charAt(0);
+            if (variable2 == 'S') {
+                stop = Instant.now(); //to obtain the current instant from the system UTC clock.
+                System.out.println("Watch Stopped");
+            }
+            double elapsedTime = Duration.between(start,stop).toMillis(); // toMillis() is used to get the value of this duration in number of milli-seconds
+            System.out.println("Time elapsed is: "+ elapsedTime/1000 + "  seconds");
         }
 
-        String couponCode=sb.toString();  // tostring is built method used to return a string.
-        System.out.println("Coupon Code: "+couponCode);
     }
 
-}
+
